832c4cd08f1a2324abcef0faea595a855fb05f4e
bojkel <marblezstars@gmail.com>
2021-04-16 23:03:42 +0300

    added some new files, connected atlas to node app

---
+++ controllers/authentication_controller.js
@@ -0,0 +1,6 @@
+const User = require('../models/user');
+const mongoose = require('mongoose');
+
+exports.register = (req,res)=>{
+    User.find({})
+}
---
+++ models/household.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const HouseholdSchema = new mongoose.Schema({
+    
+})
