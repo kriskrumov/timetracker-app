568f8d50e161401bff2656fedbe554eef83c1754
KristiyanKrumov <kristiyankrumov96@gmail.com>
2021-04-20 15:41:41 +0200

    Household model changed - added city, postcode and displaying them in home.ejs

--- models/household.js
+++ models/household.js
@@ -4,6 +4,8 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const HouseholdSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     address: String,
+    city: String,
+    postcode: String,
     userID: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
