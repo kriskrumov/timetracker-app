1bfae7340821f707d3106f79a94e0b348b182908
KristiyanKrumov <kristiyankrumov96@gmail.com>
2021-04-20 14:33:56 +0200

    Shows the current loggedin user, Household model added userId, Displays the address of the current user

--- models/household.js
+++ models/household.js
@@ -3,7 +3,11 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const HouseholdSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    address: String
+    address: String,
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 module.exports = mongoose.model('household', HouseholdSchema);
